ca9319b53f159d7061e8b0f460b8421c3e35f00a
Blake <blake.p.preston@gmail.com>
2021-01-30 16:35:33 -0600

    NavBar Adjustments

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-// When the user scrolls the page, execute myFunction
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+
+// // Get the navbar
+// var navbar = document.getElementById("navBar");
+
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+// **********************************************************
+window.onload=function(){
+  w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+  var targetWidth = 768;
+  if ( w >= targetWidth) {     
+    //Add your javascript for screens wider than or equal to 768 here
+    // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
@@ -15,3 +39,7 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+  }
+ 
+  };
+// **********************************************************
