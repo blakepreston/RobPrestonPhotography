32d46733ff631ae39159c882214d3819d1ba0144
Blake <blake.p.preston@gmail.com>
2021-01-31 13:27:25 -0600

    Mobile Updates

--- main.js
+++ main.js
@@ -43,3 +43,39 @@ function myFunction() {
  
   };
 // **********************************************************
+
+let menu_icon = document.getElementById("navBarMobile");
+let side_nav = document.getElementById("side_nav");
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+
+side_nav.style.right = "-200px";
+
+menu_icon.onclick = function () {
+  if (side_nav.style.right == "-200px") {
+    side_nav.style.right = "0";
+  } else {
+    side_nav.style.right = "-200px";
+  }
+};
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navTop").style.top = "0";
+  } else {
+    document.getElementById("navTop").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
